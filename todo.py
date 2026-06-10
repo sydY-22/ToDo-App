94450e1216598c7b4777c2d50b08782dc3495805
Sydney <sydneyb31@me.com>
2026-06-10 17:49:05 -0400

    added a way to list todos
    Signed-off-by: Sydney <sydneyb31@me.com>

--- todo.py
+++ todo.py
@@ -37,13 +37,49 @@ class ToDo:
         
         print(f"ToDo Added!: {new_todo["Title"]} -  {new_todo['Description']}")
         print()
+    
+    def list_todo(self):
+        """List all todos."""
+        print("List of ToDo's: ")
+
+        with open("todo-list.json", "r", encoding="utf-8") as data:
+            file_json = json.load(data)
+        
+        for value in file_json.values():
+            print(f"Title: {value["Title"]} - Description: {value["Description"]}")
+        
+        print()
+    
+    def delete_todo(self):
+        
+
+    def menu(self):
+        """Display menu options."""
+        print("1. List ToDo's")
+        print("2. Create ToDo")
+        print("3. Delete ToDo")
+        print("4. Exit!")
 
 
 
 def main():
     test = ToDo()
     test.check_file()
-    test.create_todo()
+
+    while True:
+        test.menu()
+        prompt = input("Choose between options 1-4: ")
+
+        if prompt == '1':
+            test.list_todo()
+        elif prompt == '2':
+            test.create_todo()
+        elif prompt == '3':
+            print('delete')
+        else:
+            False
+        
+    
 
 
 if __name__ == "__main__":
